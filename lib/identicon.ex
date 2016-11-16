0cebf96eca994047ef41a8a9159f5798841133bc
iross <iross@taskstream.com>
2016-11-16 09:06:44 -0500

    flattening and filtering grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,12 +4,26 @@ defmodule Identicon do
    |> hash_input 
    |> pick_color
    |> build_grid
+   |> filter_grid
   end
 
-  def build_grid(%Identicon.Image{hex: hex}) do
-    hex
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid = 
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def filter_grid(%Identicon.Image{grid: grid} = image) do
+   grid = Enum.filter grid, fn({fst, _}) -> 
+     rem(fst, 2) == 0
+   end 
+
+   %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
